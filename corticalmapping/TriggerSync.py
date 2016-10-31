3a288c9c8bc37dd701760ecb2c31b87767a69752
zhuangjun1981 <zhuangjun1981@gmail.com>
2016-10-31 12:35:38 -0700

    start the TriggerSync branch

---
+++ corticalmapping/TriggerSync.py
@@ -0,0 +1,11 @@
+'''
+this file is to implement master trigger mechanism and digital barcode sync signal to manage multiple online
+acquisition systems.
+'''
+
+class TriggerSequence(object):
+    pass
+
+
+class DigtalBarcode(object):
+    pass
